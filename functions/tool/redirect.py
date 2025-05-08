249f9a26643cb24d5d3072be687e3f53485c1c23
taichikuji <ivanperezfernandezsuper+github@gmail.com>
2025-05-08 11:55:06 +0200

    Edit reply function on redirect.py to mention user
    This will generate a change to avoid confusion. As it currently stands, the bot removes the original message. This is by design.
    
    In order to avoid context or confusion the bot will no longer just paste the URL and instead will mention whomever sent the original message, as well as modify the URL accordingly.

--- functions/tool/redirect.py
+++ functions/tool/redirect.py
@@ -39,7 +39,7 @@ class ReplaceCog(commands.Cog):
         if 'http://' not in message.content and 'https://' not in message.content:
             return
         if (fixed := self.replace_text(message.content)) != message.content:
-            await message.channel.send(fixed)
+            await message.channel.send(f"{message.author.mention} sent:\n{fixed}")
             try:
                 await message.delete()
             except Exception:
